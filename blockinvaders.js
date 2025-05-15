0dcf4ab0693138137dd09851fef759e4ade3888b
Zane <103996217+zaneainsworth@users.noreply.github.com>
2025-05-15 16:45:27 +0100

    Update blockinvaders.js

--- blockinvaders.js
+++ blockinvaders.js
@@ -79,30 +79,7 @@ document.addEventListener('keyup', (e) => {
 });
 
 // Touch controls for mobile
-// Detect if on mobile device
-var hasTouchScreen = false;
 
-if ("maxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.maxTouchPoints > 0;
-} else if ("msMaxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0;
-} else {
-    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-    if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-    } else if ('orientation' in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-        // Only as a last resort, fall back to user agent sniffing
-        var UA = navigator.userAgent;
-        hasTouchScreen = (
-            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-    }
-}
-
-if (hasTouchScreen) {
     let touchStartX = null;
 
     document.getElementById('controlInstructions').innerText = "Touch Controls: Left side of the screen to go left, right side to go right, tap to shoot";
@@ -134,7 +111,7 @@ if (hasTouchScreen) {
     canvas.addEventListener('touchend', (e) => {
         touchStartX = null;
     });
-}
+
 
 //Smooth Movement
 function handlePlayerMovement() {
